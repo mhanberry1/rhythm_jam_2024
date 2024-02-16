d4a586be887b5014a77740319a42e6881e375f5e
Madison Hanberry <mhanberry1@gmail.com>
2024-02-16 00:16:06 -0800

    clean up boba balls and vfx words

---
+++ Assets/Scripts/Core/Expire.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Expire : MonoBehaviour
+{
+	[SerializeField] private float Ttl = 10;
+
+    void Update()
+    {
+		if (Ttl < 0) Destroy(gameObject);
+
+		Ttl -= Time.deltaTime;
+    }
+}
