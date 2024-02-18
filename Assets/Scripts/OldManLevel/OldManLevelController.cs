a2c983789c9ef34e710dc608fd1ce0cb9fd2f4cd
KumoriKuma <francis.yunfeng.ge@gmail.com>
2024-02-18 02:37:49 -0500

    Added disco ball switching to beat

--- Assets/Scripts/OldManLevel/OldManLevelController.cs
+++ Assets/Scripts/OldManLevel/OldManLevelController.cs
@@ -20,9 +20,14 @@ public class OldManLevelController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CallResponseGameplayManager.Instance.OnBeat += OnBeat;
     }
 
+    private void OnBeat(object sender, int e)
+    {
+        AdvanceDiscoBallFrame();
+    }
+    
     void Update()
     {
         // Get time to nearest beat. The closer to a beat, the bigger the scale
@@ -44,7 +49,16 @@ public class OldManLevelController : MonoBehaviour
     void FixedUpdate()
     {
         DiscoLights.transform.Rotate(Vector3.forward, DiscoLightsRotationSpeed);
-        
-        
+    }
+
+    void AdvanceDiscoBallFrame()
+    {
+        foreach (GameObject discoBallSprite in DiscoBallSprites)
+        {
+            discoBallSprite.SetActive(false);
+        }
+
+        _currentDiscoBallFrame = (_currentDiscoBallFrame + 1) % DiscoBallSprites.Count;
+        DiscoBallSprites[_currentDiscoBallFrame].SetActive(true);
     }
 }
