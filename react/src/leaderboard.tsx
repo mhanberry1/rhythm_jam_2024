1cbb24992473ef160c150b9beee9edd72f6fa794
KumoriKuma <francis.yunfeng.ge@gmail.com>
2024-02-18 05:43:53 -0500

    Finished up the leaderboard UI in the main menu

--- react/src/leaderboard.tsx
+++ react/src/leaderboard.tsx
@@ -4,41 +4,41 @@ import "./index.scss";
 import Button from "./button";
 
 interface LeaderboardScores {
-    LatestScore: number,
-    LatestRanking: number,
-    BestScore: number,
-    BestRanking: number,
-    GlobalBestScore: number,
+  LatestScore: number;
+  LatestRanking: number;
+  BestScore: number;
+  BestRanking: number;
+  GlobalBestScore: number;
 }
 
 export default function Leaderboard() {
-    const globals = useGlobals();
-    const gameLifecycleManager = globals.gameLifecycleManager;
-    const leaderboardScores = useReactiveValue(globals.leaderboardScores) as LeaderboardScores;
-    const {
-        LatestScore,
-        LatestRanking,
-        BestScore,
-        BestRanking,
-        GlobalBestScore,
-    } = leaderboardScores;
+  const globals = useGlobals();
+  const gameLifecycleManager = globals.gameLifecycleManager;
+  const leaderboardScores = useReactiveValue(
+    globals.leaderboardScores
+  ) as LeaderboardScores;
+  const {
+    LatestScore,
+    LatestRanking,
+    BestScore,
+    BestRanking,
+    GlobalBestScore,
+  } = leaderboardScores;
 
-    return (
-        <view className="leaderboard">
-          <view className="title">Leaderboard</view>
-          <view className="content">
-            <view className="score">LatestScore: {LatestScore}</view>
-            <view className="score">LatestRanking: {LatestRanking}</view>
-            <view className="score">BestScore: {BestScore}</view>
-            <view className="score">BestRanking: {BestRanking}</view>
-            <view className="score">GlobalBestScore: {GlobalBestScore}</view>
-          </view>
-          <Button
-            text="Main Menu"
-            onClick={() => {
-              gameLifecycleManager.ReturnToMainMenu();
-            }}
-          />
-        </view>
-    );
+  return (
+    <view className="leaderboard">
+      <view className="title">Leaderboard</view>
+      <view className="content">
+        <view className="score">{`My Best Score:  ${BestScore}`}</view>
+        <view className="score">{`My Best Ranking:  #${BestRanking}`}</view>
+        <view className="score">{`World Top Score:  ${GlobalBestScore}`}</view>
+      </view>
+      <Button
+        text="Main Menu"
+        onClick={() => {
+          gameLifecycleManager.ReturnToMainMenu();
+        }}
+      />
+    </view>
+  );
 }
